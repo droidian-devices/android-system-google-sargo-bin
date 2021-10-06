f7ed02283a4233c92ceca53f278e664708cbb355
Eugenio Paolantonio (g7) <me@medesimo.eu>
2021-10-06 20:39:06 +0200

    [android-system-gsi-28-bin] Move variables to a separate configuration file, allow downloading .img files, allow checking their signature and download halium-9.0-droidian/20211002 @ g7
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- download.sh
+++ download.sh
@@ -2,13 +2,53 @@
 
 set -e
 
-CI_JOB_ID="lastSuccessfulBuild"
-ARCH="arm64"
+source ./download.conf
 
-DOWNLOAD_URL="https://ci.ubports.com/job/UBportsCommunityPortsJenkinsCI/job/ubports%252Fcommunity-ports%252Fjenkins-ci%252Fgeneric_${ARCH}/job/main/${CI_JOB_ID}/artifact/halium_halium_${ARCH}.tar.xz"
+info() {
+	echo "I: $@"
+}
+
+warning() {
+	echo "W: $@" >&2
+}
+
+error() {
+	echo "E: $@" >&2
+	exit 1
+}
+
+tmpdir="$(mktemp -d)"
+cleanup() {
+	rm -rf "${tmpdir}"
+}
+trap cleanup EXIT
 
 rm -rf downloaded_artifacts
 mkdir -p downloaded_artifacts
 
-curl -L "${DOWNLOAD_URL}" |
-	tar xJvf - -C "${PWD}/downloaded_artifacts"
+if [[ "${DOWNLOAD_URL}" == *.tar.xz ]]; then
+	info "Selected download url is a .tar.xz archive"
+	curl -L "${DOWNLOAD_URL}" |
+		tar xJvf - -C "${PWD}/downloaded_artifacts"
+
+	find ${PWD}/downloaded_artifacts -iname \*.img -exec mv \{} ${PWD}/downloaded_artifacts/android-rootfs.img \; -quit
+elif [[ "${DOWNLOAD_URL}" == *.img ]]; then
+	info "Selected download url is an .img file"
+	curl -L "${DOWNLOAD_URL}" > "${PWD}/downloaded_artifacts/android-rootfs.img"
+fi
+
+if [ -n "${SIGNATURE_DOWNLOAD_URL}" ]; then
+	info "Downloading signature"
+
+	curl -L "${SIGNATURE_DOWNLOAD_URL}" > "${PWD}/downloaded_artifacts/android-rootfs.img.asc"
+
+	# Import keys
+	for key in ${PWD}/keys/*; do
+		info "Importing key ${key}"
+		gpg --homedir "${tmpdir}" --import ${key}
+	done
+
+	info "Verifying signature"
+	gpg --homedir "${tmpdir}" --verify "${PWD}/downloaded_artifacts/android-rootfs.img.asc" ||
+		error "Unable to verify signature"
+fi
